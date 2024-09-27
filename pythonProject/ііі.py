f4e8f3660cfa0077d48411385a0de161dc036226
MarikNATURAL <markijan.duzerunku2020@gmail.com>
2024-09-27 19:47:20 +0300

    Update ііі.py

--- pythonProject/ііі.py
+++ pythonProject/ііі.py
@@ -1,11 +1,9 @@
-class Marik():
-    def __init__(self, super_name='', super_power='', super_time_save_world=''):
+class Marik:
+    def __init__(self, super_name='', super_power='', super_time_save_world=0):
         self.super_name = super_name
         self.super_power = super_power
         self.super_time_save_world = super_time_save_world
 
     def printe(self):
-        return print(self.super_name, self.super_power, self.super_time_save_world)
-
-m = Marik()
-m.printe()
+        """Виведення інформації про супергероя"""
+        return print(f"Супергерой: {self.super_name}, Суперсила: {self.super_power}, Кількість мобілізованих: {self.super_time_save_world}")
